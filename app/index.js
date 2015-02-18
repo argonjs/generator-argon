eec21ee8a3ef205a3fd8a5730f573edbfe60706a
DNoved1 <dnoved1@aim.com>
2015-02-18 14:40:38 -0500

    WIP

--- app/index.js
+++ app/index.js
@@ -2,10 +2,32 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var spawn = require('child_process').spawn
+var which = require('which')
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
+    try {
+        which.sync('jspm')
+    } catch(e) {
+        throw 'Jspm is required. Use `npm install -g jspm` to install it.'
+    }
+
+    try {
+        which.sync('gulp')
+    } catch(e) {
+        throw 'Gulp is required. Use `npm install -g gulp` to install it.'
+    }
+
+    var jspmConfig = require('jspm/lib/global-config').config
+
     this.pkg = require('../package.json');
+    
+    if (jspmConfig && jspmConfig.endpoints && jspmConfig.endpoints.gatech) {
+        // Do nothing
+    } else {
+        spawn('jspm', ['endpoint', 'create', 'gatech', 'jspm-github'])
+    }
   },
 
   prompting: function () {
